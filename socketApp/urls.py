59851acd7a34154b669d666b8e05907636820df0
erlankanybekov <kanybekov668@gmail.com>
2024-10-17 00:41:33 +0600

    fix

--- socketApp/urls.py
+++ socketApp/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
-    path('api/send_message/', SendMessageAPI.as_view(), name='send_message_api'),
-
 ]
